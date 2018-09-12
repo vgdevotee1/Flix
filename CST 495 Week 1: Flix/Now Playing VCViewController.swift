b1fb0a9affc2568f0f7ab8b6a0a60203486f29ba
Garrett Tibbetts <tibb5798@m506-118-26425.local>
2018-09-12 09:53:13 -0700

    Flix Part 1 Day 1

---
+++ CST 495 Week 1: Flix/Now Playing VCViewController.swift
@@ -0,0 +1,48 @@
+//
+//  Now Playing VCViewController.swift
+//  CST 495 Week 1: Flix
+//
+//  Created by Garrett Tibbetts on 9/12/18.
+//  Copyright © 2018 Garrett Tibbetts. All rights reserved.
+//
+
+import UIKit
+
+class Now_Playing_VCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&")!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with:data, options: []) as! [String: Any]
+            }
+            
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
