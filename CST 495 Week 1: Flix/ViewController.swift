298ed5c7c8a779624115673c6f53d19fd6451d89
Garrett Tibbetts <tibb5798@m506-118-26425.local>
2018-09-12 08:26:48 -0700

    Initial Commit

---
+++ CST 495 Week 1: Flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CST 495 Week 1: Flix
+//
+//  Created by Garrett Tibbetts on 9/12/18.
+//  Copyright © 2018 Garrett Tibbetts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
