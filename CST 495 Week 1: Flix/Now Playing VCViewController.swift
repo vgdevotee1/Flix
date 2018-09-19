4d2d618abc5f92e9dec7d89bc20c068bdd245f34
Garrett Tibbetts <tibb5798@m506-118-26425.local>
2018-09-19 09:51:19 -0700

    Fixing Table View Setup + Data

--- CST 495 Week 1: Flix/Now Playing VCViewController.swift
+++ CST 495 Week 1: Flix/Now Playing VCViewController.swift
@@ -8,28 +8,66 @@
 
 import UIKit
 
-class Now_Playing_VCViewController: UIViewController {
-
-    override func viewDidLoad() {
+class Now_Playing_VCViewController: UIViewController, UITableViewDataSource
+{
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var movies: [[String: Any]] = []
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&")!
+        tableView.dataSource = self
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        let task = session.dataTask(with: request) { (data, response, error) in
-            if let error = error {
+        let task = session.dataTask(with: request)
+        { (data, response, error) in
+            
+            if let error = error
+            {
                 print(error.localizedDescription)
             }
-            else if let data = data {
-                let dataDictionary = try! JSONSerialization.jsonObject(with:data, options: []) as! [String: Any]
-            }
             
+            else if let data = data
+            {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String:Any]]
+                self.movies = movies
+                self.tableView.reloadData()
+            }
+                
         }
         
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return movies.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
