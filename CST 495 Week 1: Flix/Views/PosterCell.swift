0c3d857d594de022611e9962859536a61e740d1f
Garrett Tibbetts <tibb5798@m506-118-26425.local>
2018-09-26 09:53:31 -0700

    Collection View Setup + Storyboard + Code complete

---
+++ CST 495 Week 1: Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  CST 495 Week 1: Flix
+//
+//  Created by Garrett Tibbetts on 9/26/18.
+//  Copyright © 2018 Garrett Tibbetts. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageview: UIImageView!
+}
